1f03f06e8872caf5b56abdf76e5f2b8128eb95b8
Heisaman <heyuwei.efrei@gmail.com>
2017-06-04 21:29:47 +0800

    remove client secret file from git repo

--- weibo_list.py
+++ weibo_list.py
@@ -1,8 +1,9 @@
 from weibo import Client
 
-APP_KEY = '1521216146' # app key
-APP_SECRET = '212c28b62505183820922d0f4f45e327' # app secret
-CALLBACK_URL = 'http://heisaman.xyz/callback' # callback url
+
+APP_KEY = '1521216146'  # app key
+APP_SECRET = '212c28b62505183820922d0f4f45e327'  # app secret
+CALLBACK_URL = 'http://heisaman.xyz/callback'  # callback url
 
 token = {u'access_token': u'2.001psIGCSorweBd556e626159iQzBB', u'remind_in': u'157679999', u'uid': u'1923041062', u'expires_at': 1629809571}
 c = Client(APP_KEY, APP_SECRET, CALLBACK_URL, token=token)
