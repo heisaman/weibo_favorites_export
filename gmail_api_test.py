63b4164380c0b214305bede5a0eff3b576c60d9e
Heisaman <heyuwei.efrei@gmail.com>
2017-06-05 00:11:27 +0800

    fix gmail Insufficient Permission error with send scope

--- gmail_api_test.py
+++ gmail_api_test.py
@@ -17,7 +17,7 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'New weibo notification'
 
